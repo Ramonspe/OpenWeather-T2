d77cd56d68709fbd2f0c5897acb547a141a4219c
Ramonspe <149334741+Ramonspe@users.noreply.github.com>
2023-12-03 14:45:51 -0300

    Add files via upload

---
+++ Sem FrontEnd (T2)/script.js
@@ -0,0 +1,35 @@
+// Projeto sem FrontEnd
+
+const axios = require("axios");
+const apiKey = "b8177396b86d1da655c3ca3d3288c05c";
+
+const city = "São Paulo";
+const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+axios
+.get(apiWeatherURL)
+.then(res => {
+    const cidade = res.data[0];
+        if (cidade) {
+        const {lat, lon} = cidade;
+        console.log(`As coordenadas são Latitude: ${lat} e Longitude: ${lon}`);
+        const current_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}&units=metric`;
+
+        axios
+        .get(current_url)
+        then(resWeather => {
+            const temperatura = resWeather.data.main.feels_like;
+            const descricao = resWeather.data.weather[0].description;
+            console.log(`Sensação Térmica: ${temperatura}ºC`);
+            console.log(`Descrição: ${descricao}`);
+            })
+        .catch(errorWeather => {
+            console.error('Algo deu errado:', errorWeather.message);
+        });
+        } else {
+            console.error('Cidade não encontrada.');
+        }
+        })
+        .catch(error => {
+            console.error('Algo deu errado ao fazer a requisição:', error.message);
+        });
